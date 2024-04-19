d2d61eb585c8fa1848ccda0035461c093f5a23d8
dipesh subedi <dipeshfitness74@gmail.com>
2024-04-19 19:00:04 +0545

    hello

--- condition.js
+++ condition.js
@@ -9,17 +9,98 @@
 // }
 
 // votes condition
-var age = 2;
-// console.log("enter your age:\n");
+// var age = 2;
+// // console.log("enter your age:\n");
 
-if(age >= 18){
-    console.log("you can vote");
-}
-else if(age < 5){
-    console.log("you are too young\n");
-    console.log("you cant vote");
-}
+// if(age >= 18){
+//     console.log("you can vote");
+// }
+// else if(age < 5){
+//     console.log("you are too young\n");
+//     console.log("you cant vote");
+// }
 
-else{
-console.log("you are  not eligible");
-}
+// else{
+// console.log("you are  not eligible");
+// }
+
+
+
+// console.log("hello aalu padu");
+// console.log("hello aalu padu");
+// return;
+// console.log("hello aalu padu");
+// var name = "dipesh";
+// if(name!== "dipesh"){
+//     console.log("right");
+
+// }
+
+
+// loops
+// 1. for loop
+// 2.while loop
+// 3. do while loop
+// 4. for in loop
+// 5. for of loop
+
+// m
+// const names = [ "sunday","monday","tuesday"];
+// // 
+// // names.push("friday");
+// // pop ko ni same
+
+// //unshift 
+// names.unshift("january");
+// console.log(names.length);// length le kati xa dekhauxa array size
+// const numbers = [1,2,3,4,5];
+// // using slice
+// const newSliceArray = numbers.slice(1,4);
+// console.log(newSliceArray)
+// const numbers = [1,2,3,4,5];
+// let splicedNumbers =numbers.splice(2,4,10,20);
+// console.log(splicedNumbers);
+
+// const person = {
+//     name:"dipeshSubedi",
+//     roll: 11,
+// }
+// Object.freeze(person);
+// person.name = "hello";
+// console.log(person.name)
+  // ======================CONDITION===============
+// 
+
+// const tempreature =29;
+// if(tempreature>30){
+//     console.log("its hot");
+
+// }
+// else
+// {
+//     console.log("cold");
+// // }
+
+
+
+
+// split
+// let text = "I love you babe"
+// const newArray = text.split("");
+// console.log(newArray);
+
+// ternary operators
+// const isRaining = false;
+
+// // const result =  isRaining ? " carry umbrella" // condition?
+// //  :"dont carry umbrella";
+// // console.log(result);
+
+// const result = isRaining && "carry umbrella" || "dont";
+// console.log(result);
+
+// shortCircuitEvaluation
+
+let isLoggedIn = true;
+const name = isLoggedIn && "Authentication"
+console.log(name);
---
+++ loops.js
@@ -0,0 +1,26 @@
+
+// for( var i = 0; i<=11;i++)
+// console.log(i ,"hello i am dipesh");
+// for( var i = 0; i<=100;i = i =i+5){
+//     console.log(i,"hello wrld")
+// }
+// let i = 0;
+// while(i<10){
+//     console.log("dipeshSubedi");
+//     i++;
+// }
+// let i = 0;
+// do{
+//     console.log(i,"dipeshSubedi");
+//     i++;
+// }
+// while(i<10)
+
+// // while(i<10){
+    
+   
+// // }
+
+
+
+
